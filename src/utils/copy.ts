17c075ea45ab937300148a9fd640faa525d8ddb3
Odran HUSSON <odran@webforger.fr>
2023-08-13 16:00:42 +0200

    🔄 Update copy utils to support additional browsers

--- src/utils/copy.ts
+++ src/utils/copy.ts
@@ -4,25 +4,55 @@ export const useClipboard = () => {
   const toast = useToast();
 
   const handleCopy = (text: string) => {
-    navigator.clipboard
-      .writeText(text)
-      .then(() => {
+    if (navigator.clipboard) {
+      // Use modern clipboard API if available
+      navigator.clipboard
+        .writeText(text)
+        .then(() => {
+          toast({
+            title: "Text copied to clipboard!",
+            status: "success",
+            duration: 3000,
+            isClosable: true,
+          });
+        })
+        .catch((err) => {
+          toast({
+            title: "Failed to copy text. Try manually.",
+            status: "error",
+            duration: 3000,
+            isClosable: true,
+          });
+          console.error("Failed to copy text: ", err);
+        });
+    } else {
+      // Fallback for older browsers
+      const textarea = document.createElement("textarea");
+      textarea.value = text;
+      textarea.style.position = "fixed";  // Prevent scrolling to the bottom of the page
+      document.body.appendChild(textarea);
+      textarea.select();
+
+      try {
+        document.execCommand('copy');
         toast({
           title: "Text copied to clipboard!",
           status: "success",
           duration: 3000,
           isClosable: true,
         });
-      })
-      .catch((err) => {
+      } catch (err) {
         toast({
           title: "Failed to copy text. Try manually.",
           status: "error",
           duration: 3000,
           isClosable: true,
         });
-        console.error("Failed to copy text: ", err);
-      });
+        console.error("Failed to copy text using execCommand: ", err);
+      }
+
+      document.body.removeChild(textarea);
+    }
   };
 
   return { handleCopy };
