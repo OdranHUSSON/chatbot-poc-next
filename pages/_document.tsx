10a3df663428d9f5d519c970d56caae250d1d5d5
Odran HUSSON <odran@webforger.fr>
2023-08-13 23:40:22 +0200

    🚀 Add basic PWA headers

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,15 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <title>CHAT GPT</title>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+        <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#422AFB" />
+        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <body suppressHydrationWarning={true}>
         <Main />
