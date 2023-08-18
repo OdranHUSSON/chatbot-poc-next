a6b73b5849a1e077f687ca5a037b9b0a8374c6d9
Odran HUSSON <ohusson55@gmail.com>
2023-08-18 15:51:49 +0000

    :sparkles: Added useEffect hook to fetch repos and useState to manage the repos state in RepoList component

--- src/components/git/RepoList.tsx
+++ src/components/git/RepoList.tsx
@@ -1,21 +1,77 @@
-import React from 'react';
-import { Box, Text, Button } from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
+import { Box, Text, Button, useToast, Card } from '@chakra-ui/react';
+import GitClient from '../utils/gitClient';
 
 interface RepoListProps {
-  repos: string[];
   onSelect: (repo: string) => void;
 }
 
-const RepoList: React.FC<RepoListProps> = ({ repos, onSelect }) => {
+const RepoList: React.FC<RepoListProps> = ({ onSelect }) => {
+  const [repos, setRepos] = useState<string[]>([]);
+  const toast = useToast();
+
+  useEffect(() => {
+    const fetchRepos = async () => {
+      try {
+        const gitClient = GitClient.getInstance();
+        const repos = await gitClient.repos();
+        setRepos(repos);
+      } catch (error) {
+        toast({
+          title: 'Error',
+          description: 'Failed to fetch repos',
+          status: 'error',
+          duration: 3000,
+          isClosable: true,
+        });
+      }
+    };
+
+    fetchRepos();
+  }, []);
+
+  const handleSelectRepo = (repo: string) => {
+    onSelect(repo);
+  };
+
+  const handleDeleteRepo = async (repo: string) => {
+    try {
+      const gitClient = GitClient.getInstance();
+      await gitClient.deleteRepo(repo);
+      toast({
+        title: 'Success',
+        description: 'Repo deleted successfully',
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+      });
+      setRepos(prevRepos => prevRepos.filter(r => r !== repo));
+    } catch (error) {
+      toast({
+        title: 'Error',
+        description: 'Failed to delete repo',
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
+
   return (
     <>
       {repos.map(repo => (
-        <Box key={repo} p={5} shadow="md" borderWidth="1px">
-          <Text>{repo}</Text>
-          <Button colorScheme="teal" variant="outline" mt={2} onClick={() => onSelect(repo)}>
-            Open
-          </Button>
-        </Box>
+        <Card key={repo} p={5} shadow="md" borderWidth="1px">
+          <Text fontSize="xl" fontWeight="bold">{repo}</Text>
+          <Text mt={2}>{/* Render README.md content here */}</Text>
+          <Box mt={4} display="flex" justifyContent="space-between">
+            <Button colorScheme="teal" variant="outline" onClick={() => handleSelectRepo(repo)}>
+              Select
+            </Button>
+            <Button colorScheme="red" variant="outline" onClick={() => handleDeleteRepo(repo)}>
+              Delete
+            </Button>
+          </Box>
+        </Card>
       ))}
     </>
   );
