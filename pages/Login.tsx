050b2dd8bc41b70641f256cf6ebe492d91c612d5
Odran HUSSON <ohusson55@gmail.com>
2023-08-25 16:30:34 +0200

    :sparkle: Login redirects to / if already Logged in + refacto

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -1,4 +1,3 @@
-// components/LoginPage.tsx
 import {
   Box,
   Button,
@@ -17,56 +16,69 @@ import {
 import { HorizonLogo as Logo } from '@/components/icons/Icons';
 import { OAuthButtonGroup } from "@/components/login/OAuthButtonGroup";
 import { PasswordField } from "@/components/login/PasswordField";
+import { useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 
-const LoginPage = () => (
-  <Container maxW="lg" py={{ base: '12', md: '24' }} px={{ base: '0', sm: '8' }}>
-    <Stack spacing="8">
-      <Stack spacing="6">
-        <Logo />
-        <Stack spacing={{ base: '2', md: '3' }} textAlign="center">
-          <Heading size={{ base: 'xs', md: 'sm' }}>Log in to your account</Heading>
-          <Text color="fg.muted">
-            Don't have an account? <Link href="#">Sign up</Link>
-          </Text>
-        </Stack>
-      </Stack>
-      <Box
-        py={{ base: '0', sm: '8' }}
-        px={{ base: '4', sm: '10' }}
-        bg={{ base: 'transparent', sm: 'bg.surface' }}
-        boxShadow={{ base: 'none', sm: 'md' }}
-        borderRadius={{ base: 'none', sm: 'xl' }}
-      >
+const LoginPage = () => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === 'loading') return; // Do nothing while loading
+    if (session) router.push('/'); // Redirect to `/` if authenticated
+  }, [session, status, router]);
+
+  return (
+    <Container maxW="lg" py={{ base: '12', md: '24' }} px={{ base: '0', sm: '8' }}>
+      <Stack spacing="8">
         <Stack spacing="6">
-          <Stack spacing="5">
-            <FormControl>
-              <FormLabel htmlFor="email">Email</FormLabel>
-              <Input id="email" type="email" />
-            </FormControl>
-            <PasswordField />
-          </Stack>
-          <HStack justify="space-between">
-            <Checkbox defaultChecked>Remember me</Checkbox>
-            <Button variant="text" size="sm">
-              Forgot password?
-            </Button>
-          </HStack>
-          <Stack spacing="6">
-            <Button>Sign in</Button>
-            <HStack>
-              <Divider />
-              <Text textStyle="sm" whiteSpace="nowrap" color="fg.muted">
-                or continue with
-              </Text>
-              <Divider />
-            </HStack>
-            <OAuthButtonGroup />
+          <Logo />
+          <Stack spacing={{ base: '2', md: '3' }} textAlign="center">
+            <Heading size={{ base: 'xs', md: 'sm' }}>Log in to your account</Heading>
+            <Text color="fg.muted">
+              Don't have an account? <Link href="#">Sign up</Link>
+            </Text>
           </Stack>
         </Stack>
-      </Box>
-    </Stack>
-  </Container>
-);
+        <Box
+          py={{ base: '0', sm: '8' }}
+          px={{ base: '4', sm: '10' }}
+          bg={{ base: 'transparent', sm: 'bg.surface' }}
+          boxShadow={{ base: 'none', sm: 'md' }}
+          borderRadius={{ base: 'none', sm: 'xl' }}
+        >
+          <Stack spacing="6">
+            <Stack spacing="5">
+              <FormControl>
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <Input id="email" type="email" />
+              </FormControl>
+              <PasswordField />
+            </Stack>
+            <HStack justify="space-between">
+              <Checkbox defaultChecked>Remember me</Checkbox>
+              <Button variant="text" size="sm">
+                Forgot password?
+              </Button>
+            </HStack>
+            <Stack spacing="6">
+              <Button>Sign in</Button>
+              <HStack>
+                <Divider />
+                <Text textStyle="sm" whiteSpace="nowrap" color="fg.muted">
+                  or continue with
+                </Text>
+                <Divider />
+              </HStack>
+              <OAuthButtonGroup />
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Container>
+  );
+};
 
 // Add this line to disable the layout for this page
 LoginPage.disableLayout = true;
