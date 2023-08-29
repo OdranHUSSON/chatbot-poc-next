b733d3de34c3c0b4ba59e1cbe84cd48cf37eb8a3
Odran HUSSON <ohusson55@gmail.com>
2023-08-29 16:27:11 +0100

    :sparkle: count tokens instead of blindly sending messages

--- pages/api/chatAPI.ts
+++ pages/api/chatAPI.ts
@@ -1,50 +1,68 @@
 import { ChatBody } from '@/types/types';
 import { OpenAIStream } from '@/utils/chatStream';
+import { encode } from 'gpt-tokenizer';
+
+const filterMessages = (historyMessages, tokenLimit) => {
+  let filteredMessages = historyMessages.slice(0, 2);
+  let restOfMessages = historyMessages.slice(2).reverse();
+  let totalTokens = 0;
+  let lastMessageId = null;
+
+  for (let message of restOfMessages) {
+    let messageTokens = encode(message.message).length;
+    if (totalTokens + messageTokens <= tokenLimit) {
+      totalTokens += messageTokens;
+      filteredMessages.unshift(message);
+    } else {
+      lastMessageId = message.id;
+      console.log("lastMessage", message)
+      break;
+    }
+  }
+  console.log("totalTokens", totalTokens)
+
+  return { filteredMessages, lastMessageId };
+};
 
 export const config = {
-    runtime: 'edge',
+  runtime: 'edge',
 };
 
 const handler = async (req: Request): Promise<Response> => {
-    try {
-        const { inputCode, model, apiKey, chatHistory } = (await req.json()) as ChatBody & { chatHistory: { id: number, message: string, role: 'user' | 'bot' }[] };
-        let apiKeyFinal;
+  try {
+    const { inputCode, model, apiKey, chatHistory } = (await req.json()) as ChatBody & { chatHistory: { id: number, message: string, role: 'user' | 'bot' }[] };
+    let apiKeyFinal;
 
-        if (apiKey) {
-            apiKeyFinal = apiKey;
-        } else {
-            apiKeyFinal = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
-        }
-
-        if (!apiKey) {
-            return new Response('API key not found', { status: 500 });
-        }
-
-		console.log("history", chatHistory)
-        
-        // Take the 20 latest messages from chatHistory
-        const latestChatHistory = chatHistory.slice(-20);
-
-        // Map the latestChatHistory to GPT format
-        const mappedHistory = latestChatHistory.map(item => ({
-            role: item.role === 'bot' ? 'system' : "user",
-            content: item.message
-        }));
-
-        mappedHistory.unshift({
-            role: "user",
-            content: "You're my AI assistant, you can answer in markdown (please always output code between markdown tild) and can also display graphs if I request it (line chart with the following syntax : <Linechart dataset='JSON_ENCODED_DATASET_HERE'>, send components such as Linchart in ONE line and no other message. My front end will read your message and handle the display)"
-        });
-
-		console.log("mapped", mappedHistory)
-
-        const stream = await OpenAIStream(inputCode, model, apiKeyFinal, mappedHistory);
-
-        return new Response(stream);
-    } catch (error) {
-        console.error(error);
-        return new Response('Error', { status: 500 });
+    if (apiKey) {
+      apiKeyFinal = apiKey;
+    } else {
+      apiKeyFinal = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
     }
+
+    if (!apiKey) {
+      return new Response('API key not found', { status: 500 });
+    }
+
+    const tokenLimit = 4096;
+    const { filteredMessages, lastMessageId } = filterMessages(chatHistory, tokenLimit);
+
+    const mappedHistory = filteredMessages.map(item => ({
+      role: item.role === 'bot' ? 'system' : "user",
+      content: item.message
+    }));
+
+    mappedHistory.unshift({
+      role: "user",
+      content: "You're my AI assistant, you can answer in markdown (always output code between markdown tild, no exception)"
+    });
+
+    const stream = await OpenAIStream(inputCode, model, apiKeyFinal, mappedHistory);
+
+    return new Response(stream);
+  } catch (error) {
+    console.error(error);
+    return new Response('Error', { status: 500 });
+  }
 };
 
 export default handler;
